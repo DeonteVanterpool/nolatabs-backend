26034776adcce4f1e0d85d68023aef7a51d3874b
Deonte Vanterpool <deonte.vanterpool@outlook.com>
2025-10-10 09:59:01 -0400

    moved to 8080

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ async fn main() {
         // .route("/subcription", post(handlers::post_subcription))
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
 
