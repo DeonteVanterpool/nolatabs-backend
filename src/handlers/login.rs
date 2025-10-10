f8466afad90dc2daea1ea4876957f293e9819fc5
Deonte Vanterpool <deonte.vanterpool@outlook.com>
2025-10-09 21:04:16 -0400

    modified cpu

---
+++ src/handlers/login.rs
@@ -0,0 +1,5 @@
+use axum::extract::{Path, Query, State};
+
+pub async fn login(State(state): State<AppState>) -> Json {
+
+}
--- src/state.rs
+++ src/state.rs
@@ -1,14 +1,11 @@
 use futures::future;
 use std::error::Error;
 
-use crate::{
-    models,
-};
+use crate::models;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
+    auth: firebase_auth_sdk::Auth,
 }
 
-impl AppState {
-}
-
+impl AppState {}
