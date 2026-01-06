b632bd19dbc74e7f9485dea71129275dbac9c423
Deonte Vanterpool <deonte.vanterpool@outlook.com>
2026-01-05 20:58:42 -0500

    added database tables for mls capability

--- migrations/20251025022546_init.down.sql
+++ migrations/20251025022546_init.down.sql
@@ -1,1 +1,13 @@
 -- Add down migration script here
+DROP TABLE blob_server_backups;
+DROP TABLE commits;
+DROP TABLE broadcast_messages;
+DROP TABLE unicast_messages;
+DROP TABLE mls_clients;
+DROP TABLE key_packages;
+DROP TABLE user_repos;
+DROP TABLE repos;
+DROP TABLE repo_permissions;
+DROP TABLE message_types;
+DROP TABLE users;
+
--- migrations/20251025022546_init.up.sql
+++ migrations/20251025022546_init.up.sql
@@ -1,39 +1,42 @@
 -- Table containing basic user information
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email TEXT,
+    id SERIAL PRIMARY KEY NOT NULL,
+    email TEXT UNIQUE NOT NULL,
     created TIMESTAMP,
+
+    CONSTRAINT proper_email CHECK (email ~ '^.+@.+$'),
 );
 
 CREATE TABLE subscriptions (
-    expires TIMESTAMP,
+    expires TIMESTAMP NOT NULL,
     user_id INT REFERENCES users(id),
 );
 
+-- types: proposal, commit, welcome, application
 CREATE TABLE message_types (
     id TEXT PRIMARY KEY,
     description TEXT,
 );
 
-CREATE TABLE repos (
-    id SERIAL PRIMARY KEY,
-    owner INT REFERENCES users(id),
-    name TEXT PRIMARY KEY,
-);
-
-CREATE TABLE commits (
-    id TEXT PRIMARY KEY,
-    changes INT REFERENCES messages(id),
-    repo INT REFERENCES repos(id),
-    created TIMESTAMP,
-);
+INSERT INTO message_types (id, description) VALUES 
+('proposal', 'MLS Proposal Message'),
+('commit', 'MLS Commit Message'),
+('welcome', 'MLS Welcome Message'),
+('application', 'MLS Application Message');
 
 CREATE TABLE repo_permissions (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULl,
     description TEXT,
 );
 
 INSERT INTO repo_permissions (description) VALUES ('read-only'), ('read-write');
+
+CREATE TABLE repos (
+    id TEXT UNIQUE NOT NULL,
+    owner INT REFERENCES users(id) NOT NULL CHECK (owner ~* '^((?!\/).)*$'), -- regular expression does not contain '/'
+    name TEXT PRIMARY KEY NOT NULL CHECK (owner ~* '^((?!\/).)*$') -- regular expression does not contain '/',
+    deleted TIMESTAMP,
+);
 
 CREATE TABLE user_repos (
     user_id INT REFERENCES users(id),
@@ -41,16 +44,49 @@ CREATE TABLE user_repos (
     permission_level INT REFERENCES repo_permissions(id),
 );
 
-CREATE TABLE blob_server_backups (
-    blob_server_id number,
-    related_commit text,
+CREATE TABLE key_packages (
+    user_id INT NOT NULL REFERENCES users,
+    key_package JSONB NOT NULL,
+    expiration_date TIMESTAMP,
+    deleted TIMESTAMP,
 );
 
--- MLS Encoded Messages table
-CREATE TABLE messages (
+CREAT TABLE mls_clients (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INT REFERENCES users,
+);
+
+-- MLS Encoded unicast Messages table
+CREATE TABLE unicast_messages (
     id SERIAL PRIMARY KEY,
     mls_data BYTEA NOT NULL,
     message_type TEXT NOT NULL REFERENCES message_types(id),
     created TIMESTAMP NOT NULL DEFAULT NOW(),
-    sender_id INT NOT NULL REFERENCES users(id)
+    sender_id INT NOT NULL REFERENCES mls_clients(id),
+    recipient_id INT REFERENCES mls_clients(id),
+    recipient_email TEXT
+);
+
+-- MLS Encoded broadcast Messages table
+CREATE TABLE broadcast_messages (
+    id SERIAL PRIMARY KEY,
+    mls_data BYTEA NOT NULL,
+    message_type TEXT NOT NULL REFERENCES message_types(id),
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    sender_id INT NOT NULL REFERENCES users(id),
+    repo_id INT REFERENCES repos(id),
+    recipient_email TEXT
+);
+
+CREATE TABLE commits (
+    id TEXT PRIMARY KEY,
+    changes INT REFERENCES broadcast_messages(id),
+    message INT REFERENCES broadcast_messages(id),
+    repo TEXT REFERENCES repos(id),
+    created TIMESTAMP,
+);
+
+CREATE TABLE blob_server_backups (
+    blob_server_id number,
+    related_commit text,
 );
