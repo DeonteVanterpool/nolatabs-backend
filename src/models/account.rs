ba422f686b9caa764617c5df971f9fa24e81ba82
Deonte Vanterpool <deonte.vanterpool@outlook.com>
2026-01-07 12:37:10 -0500

    added Settings model

--- src/models/account.rs
+++ src/models/account.rs
@@ -1,3 +1,42 @@
-pub struct Settings {
+use chrono::TimeDelta;
 
+#[derive(Debug, Clone)]
+pub enum CommandStyle {
+    Unix,
+    PlainEnglish,
+}
+
+#[derive(Debug, Clone)]
+pub enum AutoCommitBehaviour {
+    // automatically commit every [x] minutes
+    Timer(TimeDelta),
+    // automatically commit every [x] commits
+    Count(u32),
+    Off,
+}
+
+#[derive(Debug, Clone)]
+pub enum AutoPullBehaviour {
+    // automatically pull commits every [x] minutes
+    Timer(TimeDelta),
+    // automatically pull commits when online
+    On,
+    Off,
+}
+
+#[derive(Debug, Clone)]
+pub enum AutoPushBehaviour {
+    // automatically push commits every [x] minutes
+    Timer(TimeDelta),
+    // automatically push commits every [x] commits
+    Count(u32),
+    Off,
+}
+
+#[derive(Debug, Clone)]
+pub struct Settings {
+    pub preferred_command_style: CommandStyle,
+    pub auto_commit_behaviour: AutoCommitBehaviour,
+    pub auto_pull_behaviour: AutoPullBehaviour,
+    pub auto_push_behaviour: AutoPushBehaviour,
 }
