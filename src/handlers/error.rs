17e72ac7b93f3e4faf52353511beae96e51b27be
Deonte Vanterpool <deonte.vanterpool@outlook.com>
2026-01-29 13:03:11 -0500

    improved error from implemntation

--- src/handlers/error.rs
+++ src/handlers/error.rs
@@ -1,16 +1,19 @@
 use crate::logic::error::ServiceError;
 use crate::repository::error::RepoError;
-use reqwest::StatusCode;
+use axum::http::StatusCode;
 
 impl From<ServiceError> for StatusCode {
     fn from(value: ServiceError) -> Self {
         match value {
             ServiceError::RepositoryError(e) => match e {
                 RepoError::DuplicateEntry(_) => StatusCode::CONFLICT,
-                RepoError::ConnectionError => StatusCode::INTERNAL_SERVER_ERROR,
-                _ => StatusCode::INTERNAL_SERVER_ERROR,
+                RepoError::ConnectionError | RepoError::QueryError(_)  => StatusCode::INTERNAL_SERVER_ERROR,
+                RepoError::NotFound(_) => StatusCode::NOT_FOUND,
             },
-            _ => StatusCode::INTERNAL_SERVER_ERROR,
+            ServiceError::InvalidInput(_) => StatusCode::BAD_REQUEST,
+            ServiceError::AuthenticationError(_) => StatusCode::UNAUTHORIZED,
+            ServiceError::AuthorizationError(_) => StatusCode::FORBIDDEN,
+            ServiceError::Unknown(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
